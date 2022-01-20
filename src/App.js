337328acd94a790a596253386879fb8c62afb1a1
felixsaintgenis <saintgenisfelix@gmail.com>
2022-01-20 18:50:03 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    quote: '',
+    author: '',
+    quotes: [],
+  };
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+
+  fetchQuote = () => {
+    fetch('https://api.quotable.io/random', {
+      method: 'GET',
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          quote: data.content,
+          author: data.author,
+        });
+      });
+  };
+
+  changeNewQuote = () => {
+    console.log('new quote...');
+  };
+
+  render() {
+    return (
+      <Suspense fallback={<h1>Loading quote...</h1>}>
+        <div className="App">
+          <div id="quote-box">
+            <p id="text">{this.state.quote}</p>
+            <p id="author">{this.state.author}</p>
+            <div className="action-wrapper">
+              <div>
+                <a
+                  className="link"
+                  target="_blank"
+                  rel="noreferrer"
+                  id="tweet-quote"
+                  href="https://twitter.com/"
+                >
+                  <i className="fa fa-twitter"></i>
+                </a>
+                <a
+                  className="link"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.tumblr.com/"
+                >
+                  <i className="fa fa-tumblr"></i>
+                </a>
+              </div>
+              <button
+                className="myButton"
+                id="new-quote"
+                onClick={this.fetchQuote}
+              >
+                New Quote
+              </button>
+            </div>
+          </div>
+        </div>
+      </Suspense>
+    );
+  }
 }
 
 export default App;
